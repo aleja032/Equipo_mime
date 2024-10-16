9f45a17d8b0c5d284a9e12adbe1be29b37ddeba0
Alejandra <alejandra09fajardo@gmail.com>
2024-10-16 15:03:44 -0500

    prueba nombre de carpetas cambiadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchPosts } from './redux/slice/posts/postThunk';
-import PostCard from './components/postCard/PostCard';
+import React from 'react';
 import SideBarLeft from './components/SideBarLeft/SidebarMenu';
 import SideBarRight from './components/SideBarRight/SideBarRight';
 import SidebarCenter from './components/SideBarCenter/SideBarCenter';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-// import './bootstrap-icons/font/bootstrap-icons.css';
 
-import './App.css';
+import './assets/generalStyles/app.css';
 
 const App = () => {
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import  store  from './redux/store/store'
 import App from './App.jsx'
-import './index.css'
+import './assets/generalStyles/index.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById('root')).render(
